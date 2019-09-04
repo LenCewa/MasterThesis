836e95a3497b0fb91d7397032b9de3af8159bc5c
LenCewa <len13@hotmail.de>
2019-09-04 11:15:43 +0200

    NN learns constant function

--- jax_script.py
+++ jax_script.py
@@ -4,7 +4,7 @@
 #https://github.com/google/jax#mini-libraries
 
 from __future__ import print_function, division, absolute_import
-import jax.numpy as np
+import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax import random
 import time
@@ -22,29 +22,27 @@ def init_network_params(sizes, key):
 
 layer_sizes = [2, 4, 4, 1]
 param_scale = 0.1
-step_size = 0.0001
+step_size = 0.1
 num_epochs = 10
 batch_size = 1
 n_targets = 1
 params = init_network_params(layer_sizes, random.PRNGKey(0))
+initial_params = params
 
 
 def relu(x):
-  return np.maximum(0, x)
+  return jnp.maximum(0, x)
 
 
 def predict(params, inputval):
-  # per-example predictions
-  print("Predict the value of (x1,x2): ", inputval)
-
   # Initialize activations
   activations = inputval
   for w, b in params[:-1]:
-    outputs = np.dot(w, activations) + b
+    outputs = jnp.dot(w, activations) + b
     activations = relu(outputs)
 
   final_w, final_b = params[-1]
-  return np.dot(final_w, activations) + final_b
+  return jnp.dot(final_w, activations) + final_b
 
 try:
   # Make a batched version of the `predict` function
@@ -57,28 +55,51 @@ def accuracy(params, inputvals, targets):
 
 
 def loss(params, input, target):
-  preds = batched_predict(params, input)
-  return np.abs(preds) - np.abs(target)
+  print("loss::input ", input)
+  print("loss::target ", target)
+  preds = predict(params, input) #batched_predict(params, input)
+  squared_loss = jnp.power(preds - target, 2)[0]
+  return squared_loss
 
 
-@jit
+#@jit
 def update(params, x, y):
-  grads = grad(loss)(params, x, y)
-  return [(w - step_size * dw, b - step_size * db)
-          for (w, b), (dw, db) in zip(params, grads)]
+  print("Params: ", params)
+  grads = grad(loss, argnums=0)(params, x, y)
+  print("Gradients: ", grads)
+  #print("Params: ", params)
+  result = []
+  print("Zipped Grads: ", list(zip(params, grads)))
+  for (w, b), (dw, db) in zip(params, grads):
+    result += [(w - step_size * dw, b - step_size * db)]
+  return result
 
-training_generator = random.normal(random.PRNGKey(0), (10, 2 * 1))
-train_values = np.array(training_generator)
-train_labels = np.array(np.ones(10)) # constant function
+training_batch_size = 100
+training_generator = random.normal(random.PRNGKey(0), (training_batch_size, 2 * 1))
+train_values = jnp.array(training_generator)
+train_labels = jnp.array(jnp.ones(training_batch_size)) # constant function
+data = zip(train_values, train_labels)
+#print("data: ", list(data))
 
-test_generator = random.normal(random.PRNGKey(1), (10, 2 * 1))
-test_values = np.array(test_generator)
-test_labels = np.array(np.ones(10)) # constant function
+test_batch_size = 3
+test_generator = random.normal(random.PRNGKey(1), (test_batch_size, 2 * 1))
+test_values = jnp.array(test_generator)
+#test_labels = jnp.array(jnp.ones(test_batch_size)) # constant function
 
-for epoch in range(num_epochs):
-  start_time = time.time()
-  for x, y in training_generator:
-    params = update(params, x, y)
-  epoch_time = time.time() - start_time
+for x, y in data:
+  params = update(params, x, y)
 
-print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+print("DONE")
+
+# Check if params changed
+
+print("Initial Params: ", initial_params)
+print("Updated Params: ", params)
+
+# Test accuracy
+print(test_values)
+test_results = []
+for x in test_values:
+  test_results += [predict(params, x)]
+
+print("Test results: ", test_results)
--- vmap_script.py
+++ vmap_script.py
@@ -1,12 +1,19 @@
-import numpy as np
+import jax.numpy as jnp
 from jax import vmap
 
-a = np.array([1, 2, 3])
-m1 = np.array([[1, 1, 1], [2, 2, 2]])
+a = jnp.array([[1, 2, 3]]).transpose()
+b = jnp.array([1, 2, 3])
+m1 = jnp.array([[1, 2, 1], [2, 2, 2]])
+m2 = jnp.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
 
-def dotProduct(u, v):
-    return np.vdot(u, v)
+vv = lambda x, y: jnp.vdot(x, y)
+mv1 = vmap(vv, (0, None), 0)
+mv2 = vmap(vv, (None, 0), 0)
+mm1 = vmap(mv1, (None, 1), 1) #(1, 0) (None, 1)
+mm2 = vmap(mv2, (1, 0), 0)
 
-mv = vmap(dotProduct, (0, None), 0)
+#print("mv1", mv1(m1, b))
+#print("mv2", mv2(m1, b))
+print("mm1", mm1(m1, m2))
+#print("mm2", mm2(m1, a))
 
-print(mv(m1, a))
