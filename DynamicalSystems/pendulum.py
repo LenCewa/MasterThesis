9b652d5b13ec88deb8f3d9ac0852908f9bd21aeb
LenCewa <len13@hotmail.de>
2019-09-11 13:25:55 +0200

    Framework for solving Eigenfunctions

--- DynamicalSystems/pendulum.py
+++ DynamicalSystems/pendulum.py
@@ -1,5 +1,5 @@
 # https://de.wikipedia.org/wiki/Mathematisches_Pendel
 # Python implementation: https://www.quora.com/How-do-I-make-a-simulation-of-a-pendulum-using-Python-or-write-the-differential-equation
-
+# S.79 Bsp. 9.10: https://www.mathematik.tu-darmstadt.de/media/analysis/lehrmaterial_anapde/hallerd/DGLSkript1819.pdf
 # BLush implements a dynamical system and samples from this her data (PendulumFn.m)
 # TODO: Implement a function that returns trajectories
--- Fourier_Koopman_eigenfunctions.py
+++ Fourier_Koopman_eigenfunctions.py
@@ -4,20 +4,17 @@ from jax import grad, jit
 from functools import partial
 from util import *
 
-# TODO:
-#  1. BLush Koopman Loss where we have K^{m} for some natural number m
-#  2. One X_trajectory vs n different X_trajectories. And how would I compute the loss? Should I average over the different computed losses?
-#  3. How does "coefficients" look like if Ψ := g = [g_1, g_2]?
+# TODO: Coefficients should be a list [g_1, g_2, ..., g_{self.dim_sub_space}]
 
 class FourierKoopmanEigenfunctions(Fourier):
-    def __init__(self, T, omega, step_size, N, iterations, X_trajectory, Y_trajectory, loss_step_size):
+    def __init__(self, T, omega, step_size, N, iterations, X_trajectory, Y_trajectory, dim_sub_space):
         Fourier.__init__(self, T, omega, step_size, N, iterations, X_trajectory, Y_trajectory)
-        self.loss_step_size = loss_step_size
+        self.dim_sub_space = dim_sub_space
 
     def KoopmanLoss(self):
         '''
-        Computes the loss_step_size-Koopman-loss for given Fourier-polynomials, where Ψ := g = [g_1, g_2] is a vector of Fourier-polynomials of degree N
-        :return: 0.5 * [(g(x_2) - K^{self.loss_step_size} * g(x_1))² + ... + (g(x_n) - K^{self.loss_step_size} * g(x_{n-1}))²]
+        Computes the Koopman-loss for given Fourier-polynomials, where Ψ := g = [g_1, ..., g_{self.dim_sub_space}] is a vector of Fourier-polynomials of degree N
+        :return: jnp.sum[(g(x_2) - K¹ * g(x_1))² + ... + (g(x_n) - K^{n-1} * g(x_1))²]
         '''
         return -1
 
@@ -30,6 +27,7 @@ class FourierKoopmanEigenfunctions(Fourier):
         return [a_0 - self.step_size * da_0] + [coeff - self.step_size * dcoeff for coeff, dcoeff in zip(coefficients[1:], grads[1:])]
 
     def compute_coefficients(self):
+        # TODO: Iterate over list of coefficents which has length = self.dim_sub_space
         coeffs = self.coefficients
         for i in range(self.iterations):
             coeffs = self.update(coeffs, self.tv, self.tl)
