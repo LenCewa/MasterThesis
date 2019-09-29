0486d6bba5eaadbe4ce26342d0da658858a9e4d2
LenCewa <len13@hotmail.de>
2019-09-29 03:03:08 +0200

    Weekly Pendulum, finding Eigenfunctions try no. 2 - still not working

--- weakly_pendulum_koopman_test_2.py
+++ weakly_pendulum_koopman_test_2.py
@@ -3,7 +3,7 @@ from Dynamical_Systems import weakly_pendulum
 import matplotlib.pyplot as plt
 
 # Hyperparameter
-time_index = 105  # If start value y0 = 1e-4 one can choose 210 for the time index
+time_index = 200  # If start value y0 = 1e-4 one can choose 210 for the time index
 time = weakly_pendulum.t[time_index]
 dt = 0.001
 steps = int(time / dt)
@@ -31,10 +31,59 @@ K = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, np.power(dt, 3), 3 * np.power(dt, 2) + np.power(dt, 4), -3 * np.power(dt, 2), 3 * dt + 7 * np.power(dt, 3) + np.power(dt, 5), 1 - 6 * np.power(dt, 2) + 12 * np.power(dt, 4), 0, 0, 0, 0],
               [0, 0, 0, 0, 0, np.power(dt, 3), 0, np.power(dt, 3), 1, -dt, np.power(dt, 2), 0],
               [0, 0, 0, 0, 0, -np.power(dt, 4), 0, -3 * np.power(dt, 4), -6 * dt, 1 - 6 * np.power(dt, 2), 2 * dt - 6 * np.power(dt, 3), 3 * np.power(dt, 2)],
-              [0, 0, 0, 0, 0, 0, 0, 3 * np.power(dt, 5), 15 * np.power(dt, 2) + 15 * np.power(dt, 4) + np.power(dt, 6), -6 * dt - 5 * np.power(dt, 3) + 9 * np.power(dt, 5), np.power(dt, 7), a, c],
+              [0, 0, 0, 0, 0, 0, 0, 3 * np.power(dt, 5), 15 * np.power(dt, 2) + 15 * np.power(dt, 4) + np.power(dt, 6), -6 * dt - 5 * np.power(dt, 3) + 9 * np.power(dt, 5) + np.power(dt, 7), a, c],
               [0, 0, 0, 0, 0, 0, 0, np.power(dt, 6), -20 * np.power(dt, 3) - 6 * np.power(dt, 5), 15 * np.power(dt, 2) - 5 * np.power(dt, 4) - 5 * np.power(dt, 6), b, d]])
 
-print(len(basis))
-print(K.shape)
-print(K)
 
+def euler_prediction(start_value, steps):
+    xk = start_value
+    euler_preds = [xk]
+    for s in range(steps):
+        xk = euler_preds[s]
+        euler_preds += [xk - np.sin(xk)*dt]
+    return euler_preds
+
+
+def koopman_prediction(steps, basis_vector):
+    koopman_preds = []
+    pred = 0
+    for s in range(steps):
+        for k in range(12):
+            pred += np.linalg.matrix_power(K, s)[:, basis_vector][k] * basis[k]
+        koopman_preds += [pred]
+        pred = 0
+    return np.array(koopman_preds)
+
+
+def compute_function_space_error(sin_fitted_trajectory, sin_euler_preds, koopman_preds, cutoff):
+    sin_euler_error = np.power(sin_fitted_trajectory[:(cutoff + 1)] - sin_euler_preds, 2)
+    koopman_error = np.power(sin_fitted_trajectory[:cutoff] - koopman_preds, 2)
+    return sin_euler_error, koopman_error
+
+
+def get_fitted_trajectory(shift):
+    return np.repeat(trajectory, shift)
+
+koopman_preds = koopman_prediction(steps, 1)
+euler_preds = euler_prediction(x0, steps)
+sin_euler_preds = np.sin(euler_preds) * np.cos(euler_preds)
+fitted_trajectory = get_fitted_trajectory(shift_trajectory)
+sin_fitted_trajectory = np.sin(fitted_trajectory) * np.cos(fitted_trajectory)
+sin_euler_error, koopman_error = compute_function_space_error(sin_fitted_trajectory, sin_euler_preds, koopman_preds, steps)
+
+
+# Plot result
+fig, ax1 = plt.subplots()
+ax1.set_xlabel('steps / predicted steps: ' + str(steps))
+ax1.set_ylabel('trajectory')
+ax1.plot(koopman_preds, label="Koopman")
+ax1.plot(sin_euler_preds, label="sin(Euler)")
+ax1.plot(sin_fitted_trajectory, label="sin(trajectory)")
+ax1.plot(fitted_trajectory, label="trajectory")
+'''ax2 = ax1.twinx()
+ax2.set_ylabel('MSE')
+ax2.plot(sin_euler_error, label="sin(Euler) MSE")
+ax2.plot(koopman_error, label="Koopman MSE")'''
+fig.tight_layout()
+fig.legend()
+plt.show()
